b8b28bd3c821d9c3e6ac05c5a795eaeff2bdf33c
jon <jhagg314@gmail.com>
2019-08-24 00:14:27 -0700

    wip

--- oura/models/helper.py
+++ oura/models/helper.py
@@ -4,22 +4,36 @@ import json
 logger = logging.getLogger(__name__)
 
 class OuraModel:
+    # TODO factor out common keys, like "summary_date"
     _KEYS = []
+
+    def __init__(self, json_raw=None, json_parsed=None):
+        obj = json_parsed if json_parsed is not None else json.loads(json_raw)
+        set_attrs(self, obj)
+
 
     def __str__(self):
         return ", ".join( ["{}={}".format(k, getattr(self, k)) for k in self._KEYS ] )
 
 
-def from_json(raw_json, typename: OuraModel):
+def set_attrs(instance, lookup):
+    [setattr(instance, k, lookup[k]) for k in instance._KEYS if k in lookup.keys()]
 
-    json_dict = json.loads(raw_json)
+
+def from_dict(response_dict, typename: OuraModel):
     obj = typename()
 
     for k in obj._KEYS:
-        if k in json_dict.keys():
-            setattr(obj, k, json_dict[k])
+        if k in response_dict.keys():
+            setattr(obj, k, response_dict[k])
         else:
             setattr(obj, k, None)
             logger.warning("Expected property missing from json response. property={}, class={}".format(k, typename.__class__.__name__))
     
     return obj
+
+
+def from_json(raw_json, typename: OuraModel):
+
+    json_dict = json.loads(raw_json)
+    return from_dict(json_dict, typename)
