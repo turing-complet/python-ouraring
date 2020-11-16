815d594e4b1c5f7c8297d549d8caa6eec7f45500
jon <jhagg314@gmail.com>
2020-11-15 21:23:36 -0800

    bump version and format setup.py

--- oura/__init__.py
+++ oura/__init__.py
@@ -6,7 +6,11 @@ Oura API Library
 
 ------------------
 
-It's a description for __init__.py, innit.
+Welcome to the python oura library!
+
+For more information, please check the github:
+    https://github.com/turing-complet/python-ouraring
+
 
 """
 from .auth import OAuthRequestHandler, OuraOAuth2Client, PersonalRequestHandler
--- setup.py
+++ setup.py
@@ -12,18 +12,15 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'oura'
-DESCRIPTION = 'Oura api client.'
-URL = 'https://github.com/turing-complet/python-ouraring'
-EMAIL = 'jhagg314@gmail.com'
-AUTHOR = 'Jon Hagg'
-REQUIRES_PYTHON = '>=3.7'
-VERSION = '1.0.4'
+NAME = "oura"
+DESCRIPTION = "Oura api client."
+URL = "https://github.com/turing-complet/python-ouraring"
+EMAIL = "jhagg314@gmail.com"
+AUTHOR = "Jon Hagg"
+REQUIRES_PYTHON = ">=3.6"
+VERSION = "1.1.4"
 
-REQUIRED = [
-    'requests-oauthlib'
-    'pandas'
-]
+REQUIRED = ["requests-oauthlib", "pandas"]
 
 EXTRAS = {
     # 'fancy feature': ['django'],
@@ -32,51 +29,51 @@ EXTRAS = {
 here = os.path.abspath(os.path.dirname(__file__))
 
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
 about = {}
-about['__version__'] = VERSION
+about["__version__"] = VERSION
 
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
-    user_options = [
-        ('test', None, 'Upload to test server')
-    ]
+    description = "Build and publish the package."
+    user_options = [("test", None, "Upload to test server")]
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         self.test = False
-        self.test_server = 'https://test.pypi.org/legacy/'
+        self.test_server = "https://test.pypi.org/legacy/"
 
     def finalize_options(self):
         pass
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
         if self.test:
-            self.status('Uploading the package to test server via Twine…')
-            os.system('twine upload --repository-url {} dist/*'.format(self.test_server))
+            self.status("Uploading the package to test server via Twine…")
+            os.system(
+                "twine upload --repository-url {} dist/*".format(self.test_server)
+            )
         else:
-            self.status('Uploading the package to PyPI via Twine…')
-            os.system('twine upload dist/*')
+            self.status("Uploading the package to PyPI via Twine…")
+            os.system("twine upload dist/*")
 
         # self.status('Pushing git tags…')
         # os.system('git tag v{0}'.format(about['__version__']))
@@ -87,38 +84,36 @@ class UploadCommand(Command):
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['oura.client'],
-
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
     # },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     # $ setup.py publish support.
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     cmdclass={
-        'upload': UploadCommand,
+        "upload": UploadCommand,
     },
 )
